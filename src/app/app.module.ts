35108051cc8f30f388347e53b5336e3ae8c38219
Leonardo Gomes <leo.gomesleonel@gmail.com>
2023-11-01 01:05:08 -0300

    Tarefa_6_Angular_e_Typescript

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { MenuSComponent } from './menu-s/menu-s.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+import { CalcularMediaComponent } from './calcular-media/calcular-media.component';
+import { CadastroAlunoComponent } from './cadastro-aluno/cadastro-aluno.component';
+import { ApoliceSeguroComponent } from './apolice-seguro/apolice-seguro.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CalculadoraComponent, MenuSComponent, HomeComponent, ErrorComponent, CalcularMediaComponent, CadastroAlunoComponent, ApoliceSeguroComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
